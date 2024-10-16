4d1519e1fbf1d6518519509464878fe8e79834ef
Brody234 <brodymassad@gmail.com>
2024-10-16 16:36:05 -0400

    420 Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
+"use client"
 import Image from "next/image";
+import shirt1 from "../images/shirt1.webp"
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [creditCard, setCreditCard] = useState(false)
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <h1>Welcome To ThriftMe</h1>
+      <FeaturedItem src = {shirt1} creditCard = {()=>setCreditCard(true)} />
+      {
+        creditCard && 
+        <div style = {{position: 'absolute'}}>
+          <p>Enter your credit card number</p>
+          <input></input>
+          <p>CVV/CVC</p>
+          <input></input>
+          <p>Expiration Date</p>
+          <input></input>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      }
     </div>
   );
+}
+
+function FeaturedItem({src, txt, creditCard}){
+  const popUp = () =>{
+    creditCard()
+  }
+  return(
+  <div style = {{backgroundColor: '#779988', display: "inline-block", borderRadius: '10px', padding: '20px', margin: '30px'}}>
+    <Image src = {shirt1} height = "300" ></Image>
+    <div style = {{flexDirection: 'column', display: 'flex', alignContent: 'center', alignItems: 'center'}}>
+      <p>Fight Club T Shirt: $10</p>
+      <button style = {{backgroundColor: 'orange', border: 'none', outline: 'none', padding: '3px', borderRadius: '3px', marginTop: '10px', fontSize: '20px'}} onClick = {popUp}><p>Buy Now</p></button>
+    </div>
+  </div>)
 }
